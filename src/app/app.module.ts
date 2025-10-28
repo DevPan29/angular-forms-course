5cbca139ebcfd4bef4b11cbc264f458f456b0f32
Alessandro Panarese <darenagel@hotmail.it>
2025-10-28 07:36:01 +0100

    OnlyOneErrorPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import {FileUploadComponent} from './file-upload/file-upload.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {LoginReactiveComponent} from './login-reactive/login-reactive.component';
 import {PasswordStrengthDirective} from "./directives/password-strength.directive";
+import {OnlyOneErrorPipe} from "./pipes/only-one-error.pipe";
 
 @NgModule({ declarations: [
         AppComponent,
@@ -84,7 +85,8 @@ import {PasswordStrengthDirective} from "./directives/password-strength.directiv
     MatStepperModule,
     MatProgressBarModule,
     FormsModule,
-    ReactiveFormsModule, PasswordStrengthDirective], providers: [
+    ReactiveFormsModule, PasswordStrengthDirective,
+    OnlyOneErrorPipe], providers: [
         CoursesService,
         provideHttpClient(withInterceptorsFromDi())
     ] })
