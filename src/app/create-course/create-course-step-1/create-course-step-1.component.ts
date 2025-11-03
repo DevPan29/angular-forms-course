4d1e3fc660d8088400201e0a347435655bdd1f0f
Alessandro Panarese <darenagel@hotmail.it>
2025-11-03 17:00:25 +0100

    added new components to form

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -5,6 +5,11 @@ import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import {courseTitleValidator} from "../../validators/course-title.validator";
 
+interface CourseCategory {
+  code: string;
+  description: string;
+}
+
 @Component({
   selector: 'create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
@@ -24,14 +29,22 @@ export class CreateCourseStep1Component implements OnInit {
         asyncValidators: [courseTitleValidator(this.courses)],
         updateOn: 'blur'
       }
-    ]
-  })
+    ],
+    releaseAt: [new Date(), Validators.required],
+    category: ['BEGINNER', Validators.required],
+    downloadsAllowedAt: [false, Validators.requiredTrue],
+    longDescription: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  courseCategories$: Observable<CourseCategory[]>
 
   constructor(private fb: FormBuilder, private courses: CoursesService) {
 
   }
 
   ngOnInit() {
+
+     this.courseCategories$ = this.courses.findCourseCategories();
 
   }
 
