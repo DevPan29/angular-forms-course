cecdeb389b4545d9e45220a3e648cbdfe1eb863c
Alessandro Panarese <darenagel@hotmail.it>
2025-11-05 18:24:14 +0100

    step 2

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -3,18 +3,38 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
-    selector: 'create-course-step-2',
-    templateUrl: 'create-course-step-2.component.html',
-    styleUrls: ['create-course-step-2.component.scss'],
-    standalone: false
+  selector: 'create-course-step-2',
+  templateUrl: 'create-course-step-2.component.html',
+  styleUrls: ['create-course-step-2.component.scss'],
+  standalone: false
 })
 export class CreateCourseStep2Component implements OnInit {
 
+  form = this.fb.group({
+    courseType: ['premium', Validators.required],
+    price: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern('^[0-9]*$'),]
+    ],
+    promoStartAt: [null],
+    promoEndAt: [null]
+  })
 
-  ngOnInit() {
-
-
+  constructor(private fb: FormBuilder) {
 
   }
 
+  ngOnInit() {
+    this.form.valueChanges
+    .subscribe(val => {
+      const priceControl = this.form.controls["price"];
+      if (val.courseType == "free" && priceControl.enabled) {
+        priceControl.disable({emitEvent: false});
+      } else if (val.courseType == "premium" && priceControl.disabled) {
+        priceControl.enable({emitEvent: false});
+      }
+    })
+  }
 }
--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -8,7 +8,12 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
     selector: 'create-course',
     templateUrl: './create-course.component.html',
     styleUrls: ['./create-course.component.scss'],
-    standalone: false
+    standalone: false,
+    providers: [
+      {
+        provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true},
+      }
+    ]
 })
 export class CreateCourseComponent implements OnInit {
 
